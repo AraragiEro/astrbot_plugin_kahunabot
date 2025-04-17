ce046a851c83275ea4536df1423ea806b644f4d7
Alero <zhucct34@gmail.com>
2025-04-17 09:21:05 +0800

    fix: 增加图片渲染等待时间

--- src/service/picture_render_server/picture_render.py
+++ src/service/picture_render_server/picture_render.py
@@ -80,7 +80,7 @@ class PriceResRender():
         output_path = os.path.abspath(os.path.join((tmp_path), "price_res.jpg"))
 
         # 增加等待时间到5秒，确保图表有足够时间渲染
-        pic_path = await cls.render_pic(output_path, html_content, width=550, height=720, wait_time=10)
+        pic_path = await cls.render_pic(output_path, html_content, width=550, height=720, wait_time=20)
 
         if not pic_path:
             raise KahunaException("pic_path not exist.")
