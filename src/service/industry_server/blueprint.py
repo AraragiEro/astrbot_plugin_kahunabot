976bf42889f4675a153e5137234f5da28d446d3a
Alero <zhucct34@gmail.com>
2025-04-21 15:53:27 +0800

    fix: 修复get_bp_id_by_prod_typeid未正确换行的问题
    调整了get_bp_id_by_prod_typeid中的换行格式，以提高代码可读性。保证了返回值表达式的一致缩进和清晰展示。

--- src/service/industry_server/blueprint.py
+++ src/service/industry_server/blueprint.py
@@ -54,10 +54,12 @@ class BPManager:
     @classmethod
     @lru_cache(maxsize=100)
     def get_bp_id_by_prod_typeid(cls, type_id: int) -> int:
-        return (IndustryActivityProducts
+        return (
+            IndustryActivityProducts
                  .select(IndustryActivityProducts.blueprintTypeID)
                  .where((IndustryActivityProducts.productTypeID == type_id) &
                         (IndustryActivityProducts.blueprintTypeID != 45732)).scalar()
+        )
 
     @classmethod
     @lru_cache(maxsize=100)
