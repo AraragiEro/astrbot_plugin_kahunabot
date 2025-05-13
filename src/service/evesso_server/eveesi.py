282ddaadcc34db2baeb228dd7fac42545bdd39c1
Alero <zhucct34@gmail.com>
2025-05-13 19:04:25 +0800

    fix: 修复一个循环角色认证的错误

--- src/service/evesso_server/eveesi.py
+++ src/service/evesso_server/eveesi.py
@@ -20,6 +20,7 @@ async def get_request_async(url, headers=None, params=None, log=True):
                 else:
                     response_text = await response.text()
                     if log:
+                        logger.warning(f"error when request: {url}")
                         logger.warning(f'{response.status}:{response_text}')
                     return None
     except Exception as e:
